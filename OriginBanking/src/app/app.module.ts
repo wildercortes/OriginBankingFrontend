620cbc7e5a7e41fea5151bb05bd18275366e26ba
wilder jose cortes nery <wilder.cortes1995@gmail.com>
2020-06-22 21:18:31 -0300

    Agrego componentes, servicios y modelos

--- OriginBanking/src/app/app.module.ts
+++ OriginBanking/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BalanceComponent } from './Componentes/balance/balance.component';
+import { ErrorComponent } from './Componentes/error/error.component';
+import { OperationsComponent } from './Componentes/operations/operations.component';
+import { PinComponent } from './Componentes/pin/pin.component';
+import { RetiroComponent } from './Componentes/retiro/retiro.component';
+import { SuccessComponent } from './Componentes/success/success.component';
+import { TarjetaComponent } from './Componentes/tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BalanceComponent,
+    ErrorComponent,
+    OperationsComponent,
+    PinComponent,
+    RetiroComponent,
+    SuccessComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule,
